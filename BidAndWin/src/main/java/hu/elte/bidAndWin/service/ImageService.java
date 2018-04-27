7bceaad6c6d454041de507d36385aa5279cafd14
'justanotherjoke' <david.facsko@gmail.com>
2018-04-27 13:24:46 +0200

    getAllItems(), getMyItems()

--- BidAndWin/src/main/java/hu/elte/bidAndWin/service/ImageService.java
+++ BidAndWin/src/main/java/hu/elte/bidAndWin/service/ImageService.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,11 +16,11 @@ import hu.elte.bidAndWin.repository.UserRepository;
 
 @Service
 public class ImageService {
-	
+
 	private ImageRepository imageRepository;
-	
+
 	private UserRepository userRepository;
-	
+
 	private ItemRepository itemRepository;
 
 	@Autowired
@@ -30,54 +29,28 @@ public class ImageService {
 		this.userRepository = userRepository;
 		this.itemRepository = itemRepository;
 	}
-	
-	public List<Image> getImagesByItemId(long id, User loggedInUser){
+
+	public List<Image> getImagesByItemId(long id, User loggedInUser) {
 		return imageRepository.findAllByItemId(id);
 	}
 
-/*	public Image uploadImage(Image image, User user) throws IOException, UserNotValidException {
-		if(image.getItem().getId() != user.getId()) {
-			throw new UserNotValidException();
-		}
-		System.out.println(image.getPath());
-		System.out.println(image.getItem());
-		ClassPathResource backImgFile = new ClassPathResource(image.getPath());
-		//ClassPathResource backImgFile = new ClassPathResource("C:/Users/x230/Pictures/images.jpg");
-		byte[] arrayPic = new byte[(int) backImgFile.contentLength()];
-		backImgFile.getInputStream().read(arrayPic);
-		Image blackImage = new Image(arrayPic, image.getPath(), image.getItem());
-		
-		return imageRepository.save(blackImage);
-	}*/
-	
-	/*
-	public Image uploadImage() throws IOException {
-		ClassPathResource backImgFile = new ClassPathResource("images/Cat.jpg");
-		//ClassPathResource backImgFile = new ClassPathResource("C:/Users/x230/Pictures/images.jpg");
-		byte[] arrayPic = new byte[(int) backImgFile.contentLength()];
-		backImgFile.getInputStream().read(arrayPic);
-		Image blackImage = new Image(arrayPic);
-		
-		return imageRepository.save(blackImage);
-	}*/
-	
-	public Image uploadImage(MultipartFile file, long id, User user) throws IOException, UserNotValidException, ItemNotValidException {
+	public Image uploadImage(MultipartFile file, long id, User user)
+			throws IOException, UserNotValidException, ItemNotValidException {
 		Item it = itemRepository.findById(id);
-		if(it == null) {
+		if (it == null) {
 			System.out.println("nincs ilyen item");
 			throw new ItemNotValidException();
 		}
 		System.out.println(user.getId() + "hey");
-	    if(it.getUser().getId() != user.getId() ){
-	    	System.out.println("a bejelentkezett userhez nem tartozik ilyen item");
+		if (it.getUser().getId() != user.getId()) {
+			System.out.println("a bejelentkezett userhez nem tartozik ilyen item");
 			throw new UserNotValidException();
 		}
-		Image im= new Image();
-		im.setPic(file.getBytes()); 
+		Image im = new Image();
+		im.setPic(file.getBytes());
 		im.setItem(itemRepository.findById(id));
-		
+
 		return imageRepository.save(im);
 	}
-	
-	
+
 }
