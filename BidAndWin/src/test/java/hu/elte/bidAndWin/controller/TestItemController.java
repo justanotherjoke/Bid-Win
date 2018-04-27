a760ee8dad4b56c27e9055ce4675832764a7e8e0
zoli <zoli@zoli-PC>
2018-04-27 19:34:17 +0200

    userservicetest

---
+++ BidAndWin/src/test/java/hu/elte/bidAndWin/controller/TestItemController.java
@@ -0,0 +1,15 @@
+package hu.elte.bidAndWin.controller;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestItemController {
+
+    @Test
+    public void TestUser() {
+        assertTrue(true);
+    }
+}
---
+++ BidAndWin/src/test/java/hu/elte/bidAndWin/service/TestBidService.java
@@ -0,0 +1,16 @@
+package hu.elte.bidAndWin.service;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestBidService {
+
+    @Test
+    public void TestUser() {
+        assertTrue(true);
+    }
+}
