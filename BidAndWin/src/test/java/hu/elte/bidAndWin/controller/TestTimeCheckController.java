7e85c7872576663540764b9cde89c18d920246f4
whafom <vegnerz@indamail.hu>
2018-05-20 17:28:16 +0200

    vegleges testtimecontroller

--- BidAndWin/src/test/java/hu/elte/bidAndWin/controller/TestTimeCheckController.java
+++ BidAndWin/src/test/java/hu/elte/bidAndWin/controller/TestTimeCheckController.java
@@ -1,23 +1,29 @@
 package hu.elte.bidAndWin.controller;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
 import org.junit.*;
-import static org.junit.Assert.assertEquals;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
+import org.mockito.junit.MockitoJUnitRunner;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(TimeCheckController.class)
+@RunWith(MockitoJUnitRunner.class)
 public class TestTimeCheckController {
 
-    @Autowired
-    private MockMvc mvc;
+    @InjectMocks
+    TimeCheckController timeCheckController;
 
     @Test
-    public void testgetTime_ReturnTime() {
-        assertEquals(1, 1);
+    public void testGetTime_ReturnDate() {
+        Date date = new Date();
+        Timestamp currentTime = new Timestamp(date.getTime());
+        Date currentDate = (Date) currentTime;
+        System.out.println(timeCheckController.getTime().compareTo(currentDate));
+        assertTrue((timeCheckController.getTime().compareTo(currentDate)) >= 0);
+        assertTrue((timeCheckController.getTime().compareTo(currentDate)) <= 1000);
     }
 }
